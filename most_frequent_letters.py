557c3d90b0c7103a441e26ccee4888c833645d3c
Nishu Kumari <93523544+Nishu2003@users.noreply.github.com>
2022-02-13 14:17:21 +0530

    Create most_frequent_letters.py

---
+++ most_frequent_letters.py
@@ -0,0 +1,13 @@
+print("============= PROGRAM FOR PRINTING THE LETTERS IN DECREASING FREQUENCY ======================")
+W= input('Please enter a string :  ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print("===========================================================")
+print (most_frequent(W))
