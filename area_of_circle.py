e5b5f807d70e4a24ecca4ae9e31e9f17c5a11de9
Nishu Kumari <93523544+Nishu2003@users.noreply.github.com>
2022-02-13 13:39:05 +0530

    Create area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,7 @@
+print("============= PROGRAM FOR CALCULATING THE AREA OF A CIRCLE ======================")
+print("Input the radius of the circle :",end=" ")
+r=float(input())
+PI=3.14
+area=PI * r * r
+print("==================================================================================")
+print("The ares of the circle with radius ",r," is : ",area)
